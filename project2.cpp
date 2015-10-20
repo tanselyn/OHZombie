9dc40120dbdeeaf9e00ec2d485d930713aaf71e0
tanselyn <hannah@Hannahs-MacBook-Pro-2.local>
2015-10-19 20:37:32 -0400

    main first draft

--- project2.cpp
+++ project2.cpp
@@ -8,18 +8,11 @@
 
 #include <vector>
 #include <deque>
-#include <stack>
-#include <queue>
 #include <iostream>
 #include <getopt.h>
 #include <string>
 #include "project2.h"
-#include "binary_priority_queue.h"
-#include "pairing_priority_queue.h"
-#include "poorman_priority_queue.h"
 #include "RandomZombieGenerator.h"
-#include "sorted_priority_queue.h"
-#include "eecs281_priority_queue.h"
 #include <fstream>
 #include <sstream>
 
@@ -50,6 +43,80 @@ void readInput(Player &you, int &seed, int &maxDistance,
     maxHealth = nums;
 }
 
+void fillPoormanQueue(poorman_priority_queue<Zombie*, CompareZombie> &poorQueue,
+                      std::deque<Zombie> &zombieList, const int roundCounter,
+                      int &listedZombies) {
+    if (roundCounter == 1) {
+        for (deque<Zombie>::iterator it = zombieList.begin();
+             it < zombieList.end(); ++it) {
+            poorQueue.push(&(*it));
+            ++listedZombies;
+        }
+    }
+    else {
+        while (listedZombies < zombieList.size()) {
+            poorQueue.push(&zombieList[listedZombies]);
+            ++listedZombies;
+        }
+    }
+}
+
+void fillSortedQueue(sorted_priority_queue<Zombie*, CompareZombie> &sortedQueue,
+                     std::deque<Zombie> &zombieList, const int roundCounter,
+                     int &listedZombies) {
+    if (roundCounter == 1) {
+        for (deque<Zombie>::iterator it = zombieList.begin();
+             it < zombieList.end(); ++it) {
+            sortedQueue.push(&(*it));
+            ++listedZombies;
+        }
+    }
+    else {
+        while (listedZombies < zombieList.size()) {
+            sortedQueue.push(&zombieList[listedZombies]);
+            ++listedZombies;
+        }
+    }
+    
+}
+
+void fillBinaryQueue(binary_priority_queue<Zombie*, CompareZombie> &binaryQueue,
+                     std::deque<Zombie> &zombieList, const int roundCounter,
+                     int &listedZombies) {
+    if (roundCounter == 1) {
+        for (deque<Zombie>::iterator it = zombieList.begin();
+             it < zombieList.end(); ++it) {
+            binaryQueue.push(&(*it));
+            ++listedZombies;
+        }
+    }
+    else {
+        while (listedZombies < zombieList.size()) {
+            binaryQueue.push(&zombieList[listedZombies]);
+            ++listedZombies;
+        }
+    }
+}
+
+void fillPairingQueue(pairing_priority_queue<Zombie*, CompareZombie> &pairingQueue,
+                      std::deque<Zombie> &zombieList, const int roundCounter,
+                      int &listedZombies) {
+    if (roundCounter == 1) {
+        for (deque<Zombie>::iterator it = zombieList.begin();
+             it < zombieList.end(); ++it) {
+            pairingQueue.push(&(*it));
+            ++listedZombies;
+        }
+    }
+    else {
+        while (listedZombies < zombieList.size()) {
+            pairingQueue.push(&zombieList[listedZombies]);
+            ++listedZombies;
+        }
+    }
+}
+
+
 // Getopt options for command arguments
 static struct option longopts[] = {
     {"help", no_argument, nullptr, 'h'},
@@ -58,9 +125,6 @@ static struct option longopts[] = {
     {"verbose", no_argument, nullptr, 'v'},
     {nullptr, 0, nullptr, 0}
 };
-
-// Poorman Priority Queue Implementations:
-
 
 int main(int argc, char * argv[]) {
     
@@ -84,8 +148,11 @@ int main(int argc, char * argv[]) {
     // containerMarker equals 1. If sorted, 2. If binary, 3.
     // If pairing, 4. If illegal command line input, stays 0.
     int containerMarker = 0;
+    int listedZombies = 0;
+    int activeZombies = 0;
     unsigned int statisticNum = 0;
-    unsigned int inputNum;
+    unsigned int inputNum = 0;
+    unsigned int quiverInv = 0;
     
     Player you;
     int seed = 0;
@@ -93,12 +160,21 @@ int main(int argc, char * argv[]) {
     int maxSpeed = 0;
     int maxHealth = 0;
     
-    unsigned int roundCounter = 1;
-    unsigned int roundNum = 0;
+    unsigned int roundCounter = 0;
+    unsigned int roundInput = 0;
     unsigned int randZombieNum = 0;
     unsigned int namedZombieNum = 0;
+    Zombie* eater = nullptr;
+    Zombie* target = nullptr;
     deque<Zombie> zombieList;
-    Compare order;
+    deque<Zombie> deadZombies;
+    CompareZombie order1;
+    CompareRounds order2;
+    
+    poorman_priority_queue<Zombie*, CompareZombie> poorQueue(order1);
+    sorted_priority_queue<Zombie*, CompareZombie> sortedQueue(order1);
+    binary_priority_queue<Zombie*, CompareZombie> binaryQueue(order1);
+    pairing_priority_queue<Zombie*, CompareZombie> pairingQueue(order1);
     
     // Follow command line arguments
     while ((type = getopt_long(argc, argv, "hc:s:v", longopts, &idx)) != -1) {
@@ -123,7 +199,6 @@ int main(int argc, char * argv[]) {
             case 'v':
                 verbose = true;
                 break;
-                
         }
     }
     
@@ -140,14 +215,42 @@ int main(int argc, char * argv[]) {
     
     cin >> label;
     while (label == "---") {
-        cin >> label >> roundNum;
-        if (roundNum == roundCounter) {
+        ++roundCounter;
+        
+        if (verbose) {
+            os << "Round: " << roundCounter << '\n';
+        }
+        quiverInv = you.quiverCap;
+        
+        // Update alive zombies
+        for (deque<Zombie>::iterator it = zombieList.begin();
+             it < zombieList.end(); ++it) {
+            if (it->alive) {
+                it->distance = max(0, it->distance - it->speed);
+                if (verbose) {
+                    os << "Moved: " << it->name << " (health: " << it->health << ")"
+                        << '\n';
+                }
+                if (it->distance == 0) {
+                    --you.health;
+                    if (you.health == 0) {
+                        eater = &(*it);
+                    }
+                }
+            }
+        }
+
+        cin >> label >> roundInput;
+        
+        // Update current round information
+        if (roundInput == roundCounter) {
             cin >> label >> inputNum;
             randZombieNum = inputNum;
         
             onslaught::RandomZombieStats stats{maxDistance, maxSpeed, maxHealth};
             onslaught::RandomZombieGenerator generator{seed, stats};
             
+            // Create random zombies and add them to deque
             for (int i = 0; i < randZombieNum; ++i) {
                 string name = generator.getNextZombieName();
                 int speed = generator.getNextZombieSpeed();
@@ -162,6 +265,7 @@ int main(int argc, char * argv[]) {
             namedZombieNum = inputNum;
         
             cin >> label;
+            // Create named zombies and add them to deque
             if (inputNum > 0) {
                 while (label != "---") {
                     Zombie x(label,0,0,0);
@@ -178,47 +282,157 @@ int main(int argc, char * argv[]) {
             }
         }
         
-        if (containerMarker == 1) {
-            poorman_priority_queue<Zombie*, Compare> poorQueue(order);
-            
-            for (deque<Zombie>::iterator it = zombieList.begin();
-                 it < zombieList.end(); ++it) {
-                poorQueue.push(&(*it));
+        if (verbose) {
+            for (int i = listedZombies; i < zombieList.size(); ++i) {
+                os << "Created: " << zombieList[i].name << " (distance: "
+                    << zombieList[i].distance << ")" << '\n';
             }
-            
         }
         
-        if (containerMarker == 2) {
-            
-            
+        if (you.health <= 0) {
+            os << "DEFEAT IN ROUND " << roundCounter << "! " << eater->name
+                << " ate your brains!" << '\n';
+            break;
         }
         
-        if (containerMarker == 3) {
-            
+        switch(containerMarker) {
+            case 1:
+                fillPoormanQueue(poorQueue, zombieList, roundCounter, listedZombies);
+                break;
+            case 2:
+                fillSortedQueue(sortedQueue, zombieList, roundCounter, listedZombies);
+                break;
+            case 3:
+                fillBinaryQueue(binaryQueue, zombieList, roundCounter, listedZombies);
+                break;
+            case 4:
+                fillPairingQueue(pairingQueue, zombieList, roundCounter, listedZombies);
+                break;
         }
         
-        if (containerMarker == 4) {
+        while (quiverInv > 0) {
+            switch(containerMarker) {
+                case 1:
+                    target = poorQueue.top();
+                    break;
+                case 2:
+                    target = sortedQueue.top();
+                    break;
+                case 3:
+                    target = binaryQueue.top();
+                    break;
+                case 4:
+                    target = pairingQueue.top();
+                    break;
+            }
+            --target->health;
             
-            
+            if (target->health == 0)  {
+                target->alive = false;
+                deadZombies.push_back(*target);
+                switch(containerMarker) {
+                    case 1:
+                        poorQueue.pop();
+                        break;
+                    case 2:
+                        sortedQueue.pop();
+                        break;
+                    case 3:
+                        binaryQueue.pop();
+                        break;
+                    case 4:
+                        pairingQueue.pop();
+                        break;
+                }
+            }
+            --quiverInv;
         }
-        
-        ++roundCounter;
     }
 
-    
-    
+    // All zombies are killed. You survived all rounds.
+    if (you.health > 0) {
+        os << "VICTORY IN ROUND " << roundCounter << "! " << target->name
+        << " was the last zombie. You survived with " << you.health
+        << " health left." << '\n';
+    }
     
     // If statistics specified on command line:
     if (statisticNum != 0) {
+        if (you.health <= 0) {
+            for (deque<Zombie>::iterator it = zombieList.begin();
+                 it < zombieList.end(); ++it) {
+                if (it->alive) {
+                    ++activeZombies;
+                }
+            }
+        }
+        os << "Zombies still active: " << activeZombies << '\n';
+        
+        if (!deadZombies.empty()) {
+            
+            os << "First zombies killed:" << '\n';
+            for (int i = 0; i < statisticNum; ++i) {
+                if (i >= deadZombies.size()) {
+                    break;
+                }
+                else os << deadZombies[i].name << " " << i + 1 << '\n';
+            }
+            
+            os << "Last zombies killed:" << '\n';
+            for (int i = 0; i < statisticNum; ++i) {
+                if (i >= deadZombies.size()) {
+                    break;
+                }
+                if (deadZombies.size() < statisticNum) {
+                    os << deadZombies[deadZombies.size() - 1 - i].name
+                        << " " << deadZombies.size() - i << '\n';
+                }
+                else os << deadZombies[deadZombies.size() - 1 - i].name
+                        << " " << statisticNum - i << '\n';
+            }
+        }
+        
+        os << "Most active zombies:" << '\n';
+        sort(zombieList.begin(), zombieList.end(), order2);
+        if (zombieList.size() <= statisticNum) {
+            for (deque<Zombie>::iterator it = zombieList.end() - 1;
+                 it >= zombieList.begin(); --it) {
+                os << it->name << " " << it->rounds << '\n';
+            }
+        }
+        else {
+            for (int i = 0; i < statisticNum; ++i) {
+                os << zombieList[zombieList.size() - 1 - i].name << " "
+                << zombieList[zombieList.size() - 1 - i].rounds << '\n';
+            }
+        }
+        
+        os << "Lease active zombies:" << '\n';
+        if (zombieList.size() <= statisticNum) {
+            for (deque<Zombie>::iterator it = zombieList.begin();
+                 it < zombieList.end(); ++it) {
+                os << it->name << " " << it->rounds << '\n';
+            }
+        }
+        else {
+            for (int i = 0; i < statisticNum; ++i) {
+                os << zombieList[i].name << " "
+                    << zombieList[i].rounds << '\n';
+        }
         
     }
     
-    
-    // If verbose specified on comman line:
-    if (verbose) {
-        
-        
-    }
+    cout << os.str();
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
 
--- project2.h
+++ project2.h
@@ -9,10 +9,13 @@
 #ifndef OHZombie_project2_h
 #define OHZombie_project2_h
 
-#include <vector>
 #include <deque>
-#include <stack>
 #include <string>
+#include "binary_priority_queue.h"
+#include "pairing_priority_queue.h"
+#include "poorman_priority_queue.h"
+#include "sorted_priority_queue.h"
+#include "eecs281_priority_queue.h"
 
 struct Player {
     unsigned int quiverCap;
@@ -28,14 +31,18 @@ struct Zombie {
     int speed;
     int health;
     int rounds;
+    bool alive;
+    
+    Zombie(): name("a"), distance(0), speed(0), health(0), rounds(1),
+                alive(true) {}
     
     Zombie(std::string name, int distance, int speed, int health):
             name(name), distance(distance), speed(speed),
-            health(health), rounds(0) {}
+            health(health), rounds(1), alive(true) {}
     
 };
 
-struct Compare {
+struct CompareZombie {
     bool operator() (Zombie &lhs, Zombie &rhs) {
         int lhsEta = lhs.distance / lhs.speed;
         int rhsEta = rhs.distance / rhs.speed;
@@ -49,7 +56,33 @@ struct Compare {
     }
 };
 
+struct CompareRounds {
+    bool operator() (Zombie &lhs, Zombie &rhs) {
+        if (lhs.rounds == rhs.rounds) {
+            return lhs.name < rhs.name;
+        }
+        return lhs.rounds < rhs. rounds;
+    }
+};
+
 void readInput(Player &you, int &seed, int &maxDistance,
                int &maxSpeed, int &maxHealth);
+
+void fillPoormanQueue(poorman_priority_queue<Zombie*, CompareZombie> &poorQueue,
+                      std::deque<Zombie> &zombieList, const int roundCounter,
+                      int &listedZombies);
+
+void fillSortedQueue(sorted_priority_queue<Zombie*, CompareZombie> &sortedQueue,
+                     std::deque<Zombie> &zombieList, const int roundCounter,
+                     int &listedZombies);
+
+void fillBinaryQueue(binary_priority_queue<Zombie*, CompareZombie> &binaryQueue,
+                     std::deque<Zombie> &zombieList, const int roundCounter,
+                     int &listedZombies);
+
+void fillPairingQueue(pairing_priority_queue<Zombie*, CompareZombie> &pairingQueue,
+                      std::deque<Zombie> &zombieList, const int roundCounter,
+                      int &listedZombies);
+
 
 #endif
